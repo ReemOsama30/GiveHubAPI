3cf3b620b240e9e7ccfee53143536ee13a5f7604
Reem <reem.osama3052001@gmail.com>
2024-06-01 23:21:36 +0300

    edit general response

---
+++ Clean Architecture.Application/responses/GeneralResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace Clean_Architecture.Application.responses
+{
+    public class GeneralResponse
+    {
+
+        public bool IsPass { set; get; }
+        public dynamic Message { set; get; }
+
+        //very important in front end
+        public int Status { get; set; }
+
+        public string? Token { get; set; } = null;
+
+        //in case we need the expiration date
+        public DateTime? Expired { get; set; } = null;
+    }
+}
