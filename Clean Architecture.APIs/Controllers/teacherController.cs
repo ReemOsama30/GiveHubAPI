a3942e7d40bf67a676b073541822416a19391df8
Reem <reem.osama3052001@gmail.com>
2024-06-01 22:31:29 +0300

    finish my task

---
+++ Clean Architecture.APIs/Controllers/teacherController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clean_Architecture.APIs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class teacherController : ControllerBase
+    {
+
+        //reem was here
+        //
+        //
+
+    }
+}
