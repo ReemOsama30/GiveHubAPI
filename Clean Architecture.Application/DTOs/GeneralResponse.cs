4c6ec5fac4b6aba1129ca6edc24a3f151588a864
sarah-sameh <zarasamy18@gmail.com>
2024-06-01 16:29:04 +0300

    add General Response

---
+++ Clean Architecture.Application/DTOs/GeneralResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Clean_Architecture.Application.DTOs
+{
+    public class GeneralResponse
+    {
+
+        public bool IsPass { set; get; }
+        public dynamic Message { set; get; }
+
+    }
+}
