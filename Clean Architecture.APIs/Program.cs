f6048c875806a1f8c860f004458869fb2a4aeddb
SeraviaSamuel <seravia.sameh18@gmail.com>
2024-06-03 23:33:15 +0300

    solve

--- Clean Architecture.APIs/Program.cs
+++ Clean Architecture.APIs/Program.cs
@@ -6,7 +6,6 @@ using Clean_Architecture.core.Interfaces;
 using Clean_Architecture.Infrastructure.DbContext;
 using Clean_Architecture.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
 
 
 
@@ -22,7 +21,7 @@ namespace Clean_Architecture.APIs
             builder.Services.AddControllers();
             // Add services to the container.
             builder.Services.AddAuthorization();
-https://github.com/ReemOsama30/charityPulse/pull/17/conflict?name=Clean%2BArchitecture.APIs%252FProgram.cs&ancestor_oid=96f42df9f30310bc851aaa38be458841057fd71a&base_oid=3f445db0d6b35668b710190f8789342c89dfc8ea&head_oid=6b37adc6570c3237bde19cc0b3612293e5010d07
+        https://github.com/ReemOsama30/charityPulse/pull/17/conflict?name=Clean%2BArchitecture.APIs%252FProgram.cs&ancestor_oid=96f42df9f30310bc851aaa38be458841057fd71a&base_oid=3f445db0d6b35668b710190f8789342c89dfc8ea&head_oid=6b37adc6570c3237bde19cc0b3612293e5010d07
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -42,57 +41,22 @@ https://github.com/ReemOsama30/charityPulse/pull/17/conflict?name=Clean%2BArchit
 
             builder.Services.AddScoped<DonationReportService>();
             builder.Services.AddScoped<IDonationReportRepository, DonationReportRepository>();
-            builder.Services.AddScoped<IReviewRepository, ReviewRepository>();         
+            builder.Services.AddScoped<IReviewRepository, ReviewRepository>();
             builder.Services.AddScoped<ReviewService>();
-            builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();  
-            builder.Services.AddScoped<IRepository<Project>,Repository<Project>>();
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+            builder.Services.AddScoped<IRepository<Project>, Repository<Project>>();
             builder.Services.AddScoped<IRepository<Advertisment>, Repository<Advertisment>>();
 
             builder.Services.AddScoped<projectService>();
 
             builder.Services.AddScoped<corporateService>();
-            builder.Services.AddScoped<IRepository<Corporate>,Repository<Corporate>>();
+            builder.Services.AddScoped<IRepository<Corporate>, Repository<Corporate>>();
 
             builder.Services.AddScoped<AdvertismentService>();
 
 
 
-            // ------------------------------------------------------------
-            builder.Services.AddSwaggerGen(swagger =>
-            {
-                //This�is�to�generate�the�Default�UI�of�Swagger�Documentation����
-                swagger.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Version = "v1",
-                    Title = "Charity Pulse",
-                    Description = " ITI Projrcy"
-                });
-                //�To�Enable�authorization�using�Swagger�(JWT)����
-                swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-                {
-                    Name = "Authorization",
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer",
-                    BearerFormat = "JWT",
-                    In = ParameterLocation.Header,
-                    Description = "Enter�'Bearer'�[space]�and�then�your�valid�token�in�the�text�input�below.\r\n\r\nExample:�\"Bearer�eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\"",
-                });
-                swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                    new OpenApiSecurityScheme
-                    {
-                    Reference = new OpenApiReference
-                    {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                    }
-                    },
-                    new string[] {}
-                    }
-                    });
-            });
-            //---------------------------------------------------------------
+
             builder.Services.AddCors(options =>
             {
                 options.AddPolicy("MyPolicy",
